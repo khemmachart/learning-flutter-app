233f221060161e890bf3f18ed133670f86707a4b
Kao <kao@appsynth.net>
2024-10-16 23:13:01 +0700

    Project initialization with incremental number floating button

---
+++ .dart_tool/flutter_build/32a44a752c5194ad3820b8702764e284/native_assets.d
@@ -0,0 +1,1 @@
+ /Users/appsynth/Documents/Workspace/learning-flutter-app/.dart_tool/flutter_build/32a44a752c5194ad3820b8702764e284/native_assets.yaml: 
---
+++ android/app/src/main/kotlin/dev/khemmachart/learning_flutter_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.khemmachart.learning_flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
